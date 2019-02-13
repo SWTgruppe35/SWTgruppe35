5604fa12342816290f43cb14aa0effb6efe598a4
Jannik <Jannik.lading@hotmail.dk>
2019-02-13 11:50:57 +0100

    Made TempSensorTest

---
+++ ECS.Legacy_UnitTest/TempSensorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace ECS.Legacy_UnitTest
+{
+    [TestFixture]
+    public class TempSensorTest
+    {
+        private TempSensor _uut; 
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
